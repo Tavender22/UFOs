44f236b0838c4b4722cab73ecdacbbc83c5ab724
Tavender22 <66438404+Tavender22@users.noreply.github.com>
2020-09-03 16:34:22 -0600

    updating for module 11.5 - final

--- static/js/app.js
+++ static/js/app.js
@@ -25,3 +25,27 @@ function buildTable(data) {
         
     });
 }
+
+function handleClick() {
+    // grab the datetime value from the filter.
+    let date = d3.select("#datetime").property("value");
+    let firteredData = tableData;
+    
+    //check to see if a date was entered and filter the data using that date.
+    if (date) {
+        // apply filter to the table data to only keep the rows 
+        // where the datetime value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // rebuild the table using the filtered data
+    // @NOTE: if no date was entered then filterData will
+    // just be the original tableData.
+    buildTable(filteredData);
+}
+
+// add event for a button click.
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// call the buildTable table when the page loads.
+buildTable(tableData);
