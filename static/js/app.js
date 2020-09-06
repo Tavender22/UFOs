c464c25dce9480b970a9db0aac4cf87e33b7ed82
Tavender22 <66438404+Tavender22@users.noreply.github.com>
2020-09-06 10:45:12 -0600

    updating challenge

--- static/js/app.js
+++ static/js/app.js
@@ -29,16 +29,18 @@ var filters = {};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
-  
+  if (d3.event.keyCode === 13) { 
     // 4a. Save the element that was changed as a variable.
     let changedElement = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
+    console.log(elementValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
-  
+    console.log(filterId);
+
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
@@ -48,28 +50,33 @@ function updateFilters() {
       delete filters[filterId];
     }
     // 6. Call function to apply all filters and rebuild the table
-    filterTable();
-  
+    filterTable(filterId, elementValue);
+    
+  }
   }
   
   // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
+  function filterTable(filterId, elementValue) {
   
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
-  
+    console.log(filteredData);
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
     if (filterId) {
-      filteredData = filteredData.filter("id");
-    }
-  
+      console.log(filterId);
+      // apply filter to the table data to only keep the rows 
+      // where the datetime value matches the filter value
+      filteredData = filteredData.filter(row => row[filterId] === elementValue);
+      }
+      
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  }
+    }
   
   // 2. Attach an event to listen for changes to each filter
-  d3.selectAll("input").on("change", updateFilters);
+  d3.selectAll("input").on("keypress", updateFilters);
   
   // Build the table when the page loads
   buildTable(tableData);
+  
